4be49e5357161a5155cc6b5ae3cc0704da66f03e
gaboegaleano <117790174+gaboegaleano@users.noreply.github.com>
2023-03-23 14:43:49 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+
+document.getElementById('botonVer1').addEventListener('click', function(){
+    document.getElementById('detallesTelecom').style.display = 'block';
+})
+document.getElementById('botonOcultar1').addEventListener('click', function(){
+    document.getElementById('detallesTelecom').style.display = 'none';
+})
+document.getElementById('botonVer2').addEventListener('click', function(){
+    document.getElementById('detallesAol').style.display = 'block';
+})
+document.getElementById('botonOcultar2').addEventListener('click', function(){
+    document.getElementById('detallesAol').style.display = 'none';
+})
+document.getElementById('botonVer3').addEventListener('click', function(){
+    document.getElementById('detallesMulticanal').style.display = 'block';
+})
+document.getElementById('botonOcultar3').addEventListener('click', function(){
+    document.getElementById('detallesMulticanal').style.display = 'none';
+})
